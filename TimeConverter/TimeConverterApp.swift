add2aa916efc71769d08d575ec437f307a421ce1
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-04-01 13:34:11 +0200

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Valeriia Rohatynska on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
